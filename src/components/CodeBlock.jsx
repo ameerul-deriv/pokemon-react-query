45c403b4fbb362d5d05b7e2905c181ace3308964
ameerul hady <ameerul@deriv.com>
2023-11-27 17:29:30 +0800

    fix: fonts not loading

--- src/components/CodeBlock.jsx
+++ src/components/CodeBlock.jsx
@@ -2,12 +2,7 @@ import { CodeBlock as ReactCodeBlocks, dracula } from 'react-code-blocks';
 
 const CodeBlock = ({ code }) => {
     return (
-        <div
-            key={2}
-            style={{
-                fontFamily: 'Fira Code',
-            }}
-        >
+        <div key={2} style={{ fontFamily: 'Fira Code' }}>
             <ReactCodeBlocks text={code} language='javascript' theme={dracula} />
         </div>
     );
